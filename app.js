46a2f3404ae9c516a57cdd405e89e9c634029d1a
Matt VanZoeren <66024767+MattVanZoeren@users.noreply.github.com>
2021-01-28 14:43:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(MattMongoConnect, {useNewUrlParser: true})
+mongoose.connect(${{secrets.MattMongoConnect}}, {useNewUrlParser: true})
 
 const itemsSchema = new mongoose.Schema({
   name: String
